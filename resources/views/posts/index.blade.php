5570312d6067a0616f36ff91820a19320d20ea3c
kobayashiukyo <u.k.knua0419@gmail.com>
2020-11-09 17:26:39 +0900

    add_form

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.default')
 
-{{--
-@section('title')
-Blog Posts
-@endsection
---}}
-
 @section('title', 'Blog Posts')
 
 @section('content')
-<h1>Blog Posts</h1>
+<h1>
+  <a href="{{ url('/posts/create') }}" class="header-menu">New Post</a>
+  Blog Posts
+</h1>
 <ul>
   @forelse ($posts as $post)
   <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
