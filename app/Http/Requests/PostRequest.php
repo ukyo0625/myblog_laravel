f1d3e50b9a1e871fd5467740624e552e2be0d8a6
kobayashiukyo <u.k.knua0419@gmail.com>
2020-11-10 16:28:07 +0900

    add_delete

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -6,33 +6,33 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PostRequest extends FormRequest
 {
-  /**
-   * Determine if the user is authorized to make this request.
-   *
-   * @return bool
-   */
-  public function authorize()
-  {
-    return true;
-  }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-  /**
-   * Get the validation rules that apply to the request.
-   *
-   * @return array
-   */
-  public function rules()
-  {
-    return [
-    //
-    'title' => 'required|min:3',
-    'body' => 'required'
-    ];
-  }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'title' => 'required|min:3',
+            'body' => 'required'
+        ];
+    }
 
-  public function messages() {
-    return [
-      'title.required' => 'please enter title!!!'
-    ];
-  }
+    public function messages() {
+      return [
+        'title.required' => 'please enter title!!!'
+      ];
+    }
 }
